00025231a7b5e0f9898e2ae06cff8a24b4518c7c
riccardoperra <riccardo.perra@icloud.com>
2023-01-23 23:28:32 +0100

    demo: mona sans font

--- packages/kit/dev/App.css.ts
+++ packages/kit/dev/App.css.ts
@@ -1,6 +1,11 @@
 import { globalStyle } from "@vanilla-extract/css";
 import { tokens } from "../src/foundation/contract.css";
 
+globalStyle("html, body", {
+	fontFamily:
+		'Mona Sans, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+});
+
 globalStyle("[data-cui-theme=dark] body", {
 	background: tokens.accent1,
 	color: tokens.foreground,
