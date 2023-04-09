b8a6e3e47b92daabdb9b12a04b3ae4246f617c4d
Riccardo Perra <perrariccardo0@gmail.com>
2023-04-09 09:46:20 +0200

    chore: package resolution

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -5,4 +5,11 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [solidPlugin(), vanillaExtractPlugin(), tsconfigPaths()],
+	build: {
+		rollupOptions: {
+			treeshake: false,
+		},
+		target: "esnext",
+		polyfillModulePreload: true,
+	},
 });
