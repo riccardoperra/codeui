1c96730eff17032b84f79d295f25a593eb21717f
Riccardo Perra <perrariccardo0@gmail.com>
2023-05-03 22:43:38 +0200

    chore: export breakpoint and responsive style

--- packages/kit/src/foundation/index.ts
+++ packages/kit/src/foundation/index.ts
@@ -1,3 +1,6 @@
 export { tokens as themeVars } from "./contract.css";
 export { themeTokens } from "./themes.css";
 export * as layoutVars from "./layout.css";
+
+export { breakpoints } from "./breakpoints";
+export { responsiveStyle } from "./responsive";
