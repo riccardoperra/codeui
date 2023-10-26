875ccd8793c5bd8c8768df065a55088907ba930c
Riccardo Perra <riccardo.perra@icloud.com>
2023-10-26 19:46:31 +0000

    improve dialog style

--- packages/playground-next/src/routes/dialog.tsx
+++ packages/playground-next/src/routes/dialog.tsx
@@ -1,5 +1,5 @@
 import { For } from "solid-js";
-import { Button, Dialog, DialogPanelContent } from "@codeui/kit";
+import { Button, Dialog, DialogPanelContent, DialogPanelFooter } from "@codeui/kit";
 import { createStore } from "solid-js/store";
 import { DemoSectionRow } from "~/components/ui/DemoSection";
 
@@ -11,6 +11,8 @@ export default function DialogDemo() {
 		lg: false,
 		xl: false,
 		full: false,
+		mdOverflow: false,
+		fullOverflow: false
 	});
 	return (
 		<>
@@ -46,6 +48,70 @@ export default function DialogDemo() {
 						</>
 					)}
 				</For>
+			</DemoSectionRow>
+
+			<h2>Oveflow content</h2>
+
+			<DemoSectionRow>
+
+				<Button theme={"secondary"} onClick={() => setDialogState('mdOverflow', true)}>
+					Open Dialog (md)
+				</Button>
+				<Dialog
+					open={dialogState.mdOverflow}
+					title={`Dialog title`}
+					size={'md'}
+					onOpenChange={value => setDialogState('mdOverflow', value)}
+				>
+					<DialogPanelContent>
+						{new Array(20).fill("").map(value => {
+							return (
+								<p>
+
+									Kobalte is a UI toolkit for building accessible web apps and design
+									systems with SolidJS. It provides a set of low-level UI components and
+									primitives which can be the foundation for your design system
+									implementation.
+
+								</p>
+							)
+						})}
+					</DialogPanelContent>
+					<DialogPanelFooter>
+						<Button theme={'secondary'}>Close</Button>
+						<Button theme={'primary'}>Confirm</Button>
+					</DialogPanelFooter>
+				</Dialog>
+
+				<Button theme={"secondary"} onClick={() => setDialogState('fullOverflow', true)}>
+					Open Dialog (full)
+				</Button>
+				<Dialog
+					open={dialogState.fullOverflow}
+					title={`Dialog title`}
+					size={'full'}
+					onOpenChange={value => setDialogState('fullOverflow', value)}
+				>
+					<DialogPanelContent>
+						{new Array(100).fill("").map(value => {
+							return (
+								<p>
+
+									Kobalte is a UI toolkit for building accessible web apps and design
+									systems with SolidJS. It provides a set of low-level UI components and
+									primitives which can be the foundation for your design system
+									implementation.
+
+								</p>
+							)
+						})}
+					</DialogPanelContent>
+					<DialogPanelFooter>
+						<Button theme={'secondary'}>Close</Button>
+						<Button theme={'primary'}>Confirm</Button>
+					</DialogPanelFooter>
+				</Dialog>
+
 			</DemoSectionRow>
 		</>
 	);
