ad993bf2e483453f43f0c8e0065879f2c19588b9
Riccardo Perra <perrariccardo0@gmail.com>
2023-11-12 14:23:40 +0100

    fix: improve storybook theme

--- packages/storybook/.storybook/preview.tsx
+++ packages/storybook/.storybook/preview.tsx
@@ -1,5 +1,5 @@
 import { Preview } from "storybook-solidjs";
-import { themes } from "@storybook/theming";
+import { themes, ThemeVars } from "@storybook/theming";
 import { DARK_MODE_EVENT_NAME, useDarkMode } from "storybook-dark-mode";
 import { addons } from "@storybook/addons";
 import { Component, createEffect, createSignal, FlowProps } from "solid-js";
@@ -7,6 +7,7 @@ import "./reset.css";
 import "./global.css";
 import "./global-ve.css";
 import { DocsContainer, DocsContainerProps } from "@storybook/blocks";
+import { themeVars } from "@codeui/kit";
 
 function ThemeWrapper(props: FlowProps) {
 	const [darkMode, setDarkMode] = createSignal<boolean>(true);
@@ -32,13 +33,25 @@ export const decorators = [
 	},
 ];
 
+const darkTheme: ThemeVars = {
+	...themes.dark,
+	appBg: "#111",
+	barBg: "#111",
+	appContentBg: "#050505",
+};
+
+const lightTheme: ThemeVars = {
+	...themes.normal,
+};
+
 const preview: Preview = {
 	parameters: {
 		actions: { argTypesRegex: "^on[A-Z].*" },
 		decorators: decorators,
 		darkMode: {
-			dark: { ...themes.dark, appBg: "#1c1c1c", appContentBg: "#151718" },
-			light: { ...themes.normal, appBg: "#f9fafb", appContentBg: "#ffffff" },
+			current: "dark",
+			dark: darkTheme,
+			light: lightTheme,
 		},
 		controls: {
 			matchers: {
@@ -49,7 +62,7 @@ const preview: Preview = {
 		docs: {
 			container: (props: DocsContainerProps) => {
 				const dark = useDarkMode();
-				return <DocsContainer {...props} theme={dark ? themes.dark : themes.light} />;
+				return <DocsContainer {...props} theme={dark ? darkTheme : lightTheme} />;
 			},
 		},
 	},
